aaf7f518ab6e3a0a2d19c2f1cd5e2eccbeaed277
Kun Chen <cs.kunchen@gmail.com>
2016-07-13 20:29:07 -0700

    Import Fundation instead of UIKit

--- DYCoding/DYCoding.h
+++ DYCoding/DYCoding.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 KunChen. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DYCoding.
 FOUNDATION_EXPORT double DYCodingVersionNumber;
